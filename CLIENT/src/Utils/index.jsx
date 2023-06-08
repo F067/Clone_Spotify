0932956ad39e291d35fdc03a146cf1ce4e58aadd
Fois Gregory <Gregory.Fois@nexus.int>
2023-06-08 09:33:46 +0200

    callGetAPI function

--- CLIENT/src/Utils/index.jsx
+++ CLIENT/src/Utils/index.jsx
@@ -1,18 +1,37 @@
 import axios from 'axios'
 
-export const callPost = async (url, data)=>{
-    try {
-        const response = await axios.post("http://localhost:3001" + url, data);
-        
-        if (response.status === 200) {
-          // La requête a été traitée avec succès
-          return response.data;
-        } else {
-          // La requête a échoué avec un statut autre que 200
-          throw new Error("Request failed with status: " + response.status);
-        }
-      } catch (error) {
-        // Une erreur s'est produite lors de la requête
-        throw new Error("Request failed: " + error.message);
-      }
+export const callPost = async (url, data) => {
+  try {
+    const response = await axios.post("http://localhost:3001" + url, data);
+
+    if (response.status === 200) {
+      // La requête a été traitée avec succès
+      return response.data;
+    } else {
+      // La requête a échoué avec un statut autre que 200
+      throw new Error("Request failed with status: " + response.status);
+    }
+  } catch (error) {
+    // Une erreur s'est produite lors de la requête
+    throw new Error("Request failed: " + error.message);
+  }
 }
+
+export const callGet = async (url, data) => {
+  try {
+    const response = await axios.get("http://localhost:3001" + url, { data });
+
+    if (response.status === 200) {
+      if (response.data) {
+        return response.data;
+      } else {
+        throw new Error("Response data is undefined.");
+      }
+    } else {
+      throw new Error("Request failed with status: " + response.status);
+    }
+  } catch (error) {
+    throw new Error("Request failed: " + error.message);
+  }
+};
+
